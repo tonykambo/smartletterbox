273f321268924a3f2cce1badc61c31c89a7cf528
c835722 <c835722@gmail.com>
2016-02-13 23:10:53 +1100

    This resolves enhancement #11

--- src/smartletterbox.ino
+++ src/smartletterbox.ino
@@ -10,6 +10,9 @@ extern "C" {
 }
 
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
 #include <Wire.h>
 #include "DHT.h"
@@ -59,7 +62,9 @@ int state = 0;
 
 
 void init_wifi() {
-  Serial.print("Connecting to ");
+  Serial.println("Initialising Wifi");
+  WiFi.mode(WIFI_STA);
+  Serial.println("Connecting to ");
   Serial.print(ssid);
 
   if (strcmp (WiFi.SSID().c_str(), ssid) != 0) {
@@ -73,6 +78,39 @@ void init_wifi() {
   Serial.println("");
   Serial.print("WiFi connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+// Port defaults to 8266
+// ArduinoOTA.setPort(8266);
+
+// Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(IOT_DEVICE_ID);
+
+// No authentication by default
+// ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting OTA update");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnding OTA update");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.print("Test: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 // Initialise Sensor
@@ -134,8 +172,9 @@ void loop() {
     debugDisplayPayload();
     publishPayload();
     ++counter;
-    delay(15000);
+    delay(5000);
   }
+  ArduinoOTA.handle();
 }
 
 // ************************************************************************
